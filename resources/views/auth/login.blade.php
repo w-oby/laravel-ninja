14a45089cc293974add725e87696ee5baca92f64
w-oby <wail.ouladbenyoussef@student.rocva.nl>
2026-02-13 12:22:35 +0100

    Made login + register pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,29 @@
 <x-layout>
 
-    <h2>Login</h2>
+    <form action="{{ route('login') }}" method="POST">
+    @csrf
+
+    <h2>Log In to Your Account</h2>
+
+    <label for="email">Email:</label>
+    <input 
+        type="email"
+        name="email"
+        required
+        value="{{ old('email') }}"
+    >
+
+    <label for="password">Password:</label>
+    <input 
+        type="password"
+        name="password"
+        required
+    >
+
+    <button type="submit" class="btn mt-4">Log in</button>
+
+    <!-- validation errors -->
+    
+    </form>
 
 </x-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,46 @@
 <x-layout>
 
-    <h2>Register</h2>
+    <form action="{{ route('register') }}" method="POST">
+    @csrf
+
+    <h2>Register for an Account</h2>
+
+    <label for="name">Email:</label>
+    <input 
+        type="text"
+        name="name"
+        required
+        value="{{ old('name') }}"
+
+    >
+
+    <label for="email">Name:</label>
+    <input 
+        type="email"
+        name="email"
+        required
+        value="{{ old('email') }}"
+
+    >
+
+    <label for="password">Password:</label>
+    <input 
+        type="password"
+        name="password"
+        required
+    >
+
+    <label for="password_confirmation">Confirm Password:</label>
+    <input 
+        type="password"
+        name="password_confirmation"
+        required
+    >
+
+    <button type="submit" class="btn mt-4">Register</button>
+
+    <!-- validation errors -->
+    
+    </form>
 
 </x-layout>
