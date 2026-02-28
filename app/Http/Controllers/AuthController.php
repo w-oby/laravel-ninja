d46e59e3ebec8fed410cd42adc30ff5f58638dc1
w-oby <w-oby>
2026-02-28 01:27:10 +0100

    Added login logic

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -36,10 +37,26 @@ class AuthController extends Controller
 
     }
 
-    public function login() {
+    public function login(Request $request) {
 
-    
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string'
+        ]);
 
+        if (Auth::attempt($validated)) {
+
+            $request->session()->regenerate();
+
+            return redirect()->route('ninjas.index');
+
+        }
+
+        throw ValidationException::withMessages([
+
+            'credentials' => 'Incorrect credentials'
+
+        ]);
     }
 
     public function logout(Request $request) {
