c733cf32535190aef3372e15c7062c835ebd1195
w-oby <w-oby>
2026-02-28 01:27:53 +0100

    Added error message + fixed variables

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -42,7 +42,7 @@
     <!-- validation errors -->
     @if ($errors->any())
         <ul class="px-4 py-2 bg-red-100">
-            @foreach (@error->all() as $error)
+            @foreach ($errors->all() as $error)
                 <li class="my-2 text-red-500">{{ $error }}</li>
             @endforeach
         </ul>
