7b1555f3b2cb6caa06f752cdf22fd394a13d7d64
w-oby <w-oby>
2026-03-09 11:36:36 +0100

    Wrapped the routes into middleware

--- routes/web.php
+++ routes/web.php
@@ -8,17 +8,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// GET
-Route::get('/register', [AuthController::class, 'showRegister'])->name('show.register');
-Route::get('/login', [AuthController::class, 'showLogin'])->name('show.login');
-
-// POST
-Route::post('/register', [AuthController::class, 'register'])->name('register');
-Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get('/ninjas', [NinjaController::class, 'index'])->name('ninjas.index');
-Route::get('/ninjas/create', [NinjaController::class, 'create'])->name('ninjas.create');
-Route::get('/ninjas/{ninja}', [NinjaController::class, 'show'])->name('ninjas.show');
-Route::post('/ninjas', [NinjaController::class, 'store'])->name('ninjas.store');
-Route::delete('/ninjas/{ninja}', [NinjaController::class, 'destroy'])->name('ninjas.destroy');
+Route::middleware('guest')->controller(AuthController::class)->group(function() {
+    Route::get('/register', 'showRegister')->name('show.register');
+    Route::get('/login', 'showLogin')->name('show.login');
+    Route::post('/register', 'register')->name('register');
+    Route::post('/login', 'login')->name('login');
+});
+
+Route::middleware('auth')->controller(NinjaController::class)->group(function() {
+    Route::get('/ninjas', 'index')->name('ninjas.index');
+    Route::get('/ninjas/create', 'create')->name('ninjas.create');
+    Route::get('/ninjas/{ninja}', 'show')->name('ninjas.show');
+    Route::post('/ninjas', 'store')->name('ninjas.store');
+    Route::delete('/ninjas/{ninja}', 'destroy')->name('ninjas.destroy');
+});
